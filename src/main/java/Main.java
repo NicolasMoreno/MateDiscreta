097abcf618ba643ea70a6e8722e3a0d47d7103c9
Sebas-Sirius <sebasbelaustegui@sirius.com.ar>
2017-10-23 23:03:32 -0300

    main added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,76 +1,16 @@
 import Automaton.FiniteAutomaton;
-import Automaton.FiniteAutomatonBuilder;
 import Automaton.FiniteState;
 import Automaton.FiniteTransition;
-import HtmlReader.HtmlReader;
 import extras.GraphViz;
-import guru.nidi.graphviz.attribute.Attributed;
-import extras.GraphViz;
-import guru.nidi.graphviz.attribute.Color;
-import guru.nidi.graphviz.attribute.Shape;
-import guru.nidi.graphviz.attribute.Style;
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-import static guru.nidi.graphviz.model.Factory.*;
-
-import guru.nidi.graphviz.model.*;
-
 public class Main {
     public static void main(String[] args) {
 
-        /*HtmlReader htmlReader;
-        try {
-            htmlReader = new HtmlReader("/Users/nicolasmoreno/Nico/Facultad/Mate Dis/TpMateDiscreta/src/resources/files/htmlFile.html");
-            System.out.println(htmlReader.readLine());
-            System.out.println(htmlReader.readLine());
-        } catch (IOException e2){
-            e2.printStackTrace();
-        }*/
-
-//        try{
-//            Graph g = graph("example1").directed().with(node("a").link(node("b")).link(node("c").link(node("c"))));
-//            Graphviz.fromGraph(g).width(200).render(Format.PNG).toFile(new File("example/ex1.png"));
-//        }catch (IOException e){
-//            System.out.println(e);
-//        }
-//
-//        try {
-//
-//            Node
-//                    init = node("a"),
-//                    execute = node("b"),
-//                    compare = node("c"),
-//                    mkString = node("d").with(Label.of("make a\nstring")),
-//                    printf = node("e");
-//
-//            Graph g = graph("example2").directed().with(
-//                    node("q0").link(
-//                            to(node("q1").link(execute)).with("weight", 8),
-//                            to(init),
-//                            node("q2"),
-//                            to(printf).with(Label.of("transition")),
-//                            execute.link(
-//                                    graph().with(mkString, printf),
-//                                    to(compare),
-//                                    init.link(mkString)
-//                            )
-//                    )
-//            );
-//            Graphviz.fromGraph(g).width(200).render(Format.PNG).toFile(new File("example/ex1.png"));
-//        } catch (IOException e) {
-//            System.out.println(e);
-//        }
-
-
-        /*
-         * ---------------------- OURS AUTOMATON -------------------------------
-         */
         FiniteState q0 = new FiniteState("InitialState");
 
         FiniteAutomaton automat = new FiniteAutomaton(q0);
@@ -81,35 +21,34 @@ public class Main {
         FiniteAutomaton.Result result3 = automat.evaluate("hello");
         System.out.println("Result3 valid " + result3.isValid());
 
-        graphic(automat.getInitialState());
-
+        graphic(automat);
+//        dotToPNG();
     }
 
     //TODO
-    private static void graphic(FiniteState state) {
-        List<FiniteState> states = state.getAllStates();
+    private static void graphic(FiniteAutomaton automaton) {
+        List<FiniteState> states = automaton.getAllStates();
 
         File file = new File("example/example.dot");
         try {
             FileWriter writer = new FileWriter(file);
-            writer.append("digraph { \n rankdir = \"LR\"; \n");
+            writer.append("digraph { \n\t rankdir = \"LR\"; \n");
 
             for (FiniteState finiteState: states) {
                 if(finiteState.isFinal()){
-                    writer.append("node [shape=doublecircle] Node" + finiteState.getName() + " [label =" + finiteState.getName() + "];");
+                    writer.append("\t node [shape=doublecircle] Node").append(finiteState.getName()).append(" [label =").append(finiteState.getName()).append("];\n");
                 }else{
-                    writer.append("node [shape=circle] Node").append(finiteState.getName()).append(" [label =").append(finiteState.getName()).append("];");
+                    writer.append("\t node [shape=circle] Node").append(finiteState.getName()).append(" [label =").append(finiteState.getName()).append("];\n");
                 }
             }
 
             for (FiniteState finiteState : states){
                 String nodeName = finiteState.getName();
                 for (FiniteTransition transition: finiteState.getTransitions()){
-                    writer.append("Node"+nodeName+" -> Node"+transition.getState().getName()+ "[label=["+'"'+
-                            transition.getChar()+'"'+"];");
+                    writer.append("\t Node").append(nodeName).append(" -> Node").append(transition.getState().getName()).append("[label=[").append(String.valueOf('"')).append(String.valueOf(transition.getChar())).append(String.valueOf('"')).append("];\n");
                 }
             }
-            writer.append("\n}");
+            writer.append("}");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -122,7 +61,7 @@ public class Main {
      */
     private static void dotToPNG()
     {
-        String input = "/example/example.dot";
+        String input = "example/example.dot";
 
         GraphViz gv = new GraphViz();
         gv.readSource(input);
@@ -131,7 +70,7 @@ public class Main {
         String type = "png";
         String representationType= "dot";
 
-        File out = new File("/example/example." + type);
+        File out = new File("example/example." + type);
         gv.writeGraphToFile( gv.getGraph(gv.getDotSource(), type, representationType), out);
     }
 }
